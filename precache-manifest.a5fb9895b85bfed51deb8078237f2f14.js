e53f430219f8a3781e34f690d1f8ac9297c7c00e
Keen <kwilson@finsignum.com>
2020-04-13 14:46:20 -0500

    Update burger menu

---
+++ precache-manifest.a5fb9895b85bfed51deb8078237f2f14.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "361f23a2fff514d64fa6e4a3ccaf77b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62cc8ef1f337ccee9df1",
+    "url": "/static/css/2.460fcf43.chunk.css"
+  },
+  {
+    "revision": "bbb1455b364c74611309",
+    "url": "/static/css/main.75db4a86.chunk.css"
+  },
+  {
+    "revision": "62cc8ef1f337ccee9df1",
+    "url": "/static/js/2.63d3d2ab.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.63d3d2ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbb1455b364c74611309",
+    "url": "/static/js/main.c73266a7.chunk.js"
+  },
+  {
+    "revision": "339d9f039af672a8ba0b",
+    "url": "/static/js/runtime-main.32b2e841.js"
+  },
+  {
+    "revision": "4aba96e9c23304af4b0dfc6e65ed835c",
+    "url": "/static/media/smoke-n-seoul.4aba96e9.jpg"
+  }
+]);
