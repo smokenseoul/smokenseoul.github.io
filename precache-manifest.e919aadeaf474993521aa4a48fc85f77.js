6cdb2e6fdca4aedf9b02c1b7a3ad5fd45af20ad2
Keen <kwilson@finsignum.com>
2020-04-19 20:02:15 -0500

    Remove unused components and add CNAME

---
+++ precache-manifest.e919aadeaf474993521aa4a48fc85f77.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92a793b74965a11d153a8182b69422d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62cc8ef1f337ccee9df1",
+    "url": "/static/css/2.460fcf43.chunk.css"
+  },
+  {
+    "revision": "1f53cca4956dbbce486f",
+    "url": "/static/css/main.75db4a86.chunk.css"
+  },
+  {
+    "revision": "62cc8ef1f337ccee9df1",
+    "url": "/static/js/2.63d3d2ab.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.63d3d2ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f53cca4956dbbce486f",
+    "url": "/static/js/main.0a03c59e.chunk.js"
+  },
+  {
+    "revision": "339d9f039af672a8ba0b",
+    "url": "/static/js/runtime-main.32b2e841.js"
+  },
+  {
+    "revision": "4aba96e9c23304af4b0dfc6e65ed835c",
+    "url": "/static/media/smoke-n-seoul.4aba96e9.jpg"
+  }
+]);
