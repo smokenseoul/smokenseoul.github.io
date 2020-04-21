7d9432a895b107bc45827bc1d3a9f855ffad138b
Keen <kwilson@finsignum.com>
2020-04-20 21:26:54 -0500

    Add food category tags

---
+++ precache-manifest.dcaae447f0f51dffb653d71defe9ed84.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "433d550fe16c94d995e8f505f350c4e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fe6fdcc38053fa67675",
+    "url": "/static/css/2.9bbaddc3.chunk.css"
+  },
+  {
+    "revision": "b032d3100a1894e2b750",
+    "url": "/static/css/main.b44b4f2a.chunk.css"
+  },
+  {
+    "revision": "7fe6fdcc38053fa67675",
+    "url": "/static/js/2.09d81a21.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.09d81a21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b032d3100a1894e2b750",
+    "url": "/static/js/main.1dc0b3e2.chunk.js"
+  },
+  {
+    "revision": "339d9f039af672a8ba0b",
+    "url": "/static/js/runtime-main.32b2e841.js"
+  },
+  {
+    "revision": "4aba96e9c23304af4b0dfc6e65ed835c",
+    "url": "/static/media/smoke-n-seoul.4aba96e9.jpg"
+  }
+]);
