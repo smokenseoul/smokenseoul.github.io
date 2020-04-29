1add165b218c9993c6c5b9b52ceff2f5a795f8d9
Keen <kwilson@finsignum.com>
2020-04-28 20:32:25 -0500

    Make font thicker

---
+++ precache-manifest.e5d9de3131e13dd34bc0133fdd7afba7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bbc3d0396747d25b3cb5c4b06675104",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fe6fdcc38053fa67675",
+    "url": "/static/css/2.9bbaddc3.chunk.css"
+  },
+  {
+    "revision": "689ff64a6c479fe6bfc6",
+    "url": "/static/css/main.1ea22555.chunk.css"
+  },
+  {
+    "revision": "7fe6fdcc38053fa67675",
+    "url": "/static/js/2.09d81a21.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.09d81a21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "689ff64a6c479fe6bfc6",
+    "url": "/static/js/main.a3a640c8.chunk.js"
+  },
+  {
+    "revision": "339d9f039af672a8ba0b",
+    "url": "/static/js/runtime-main.32b2e841.js"
+  },
+  {
+    "revision": "4aba96e9c23304af4b0dfc6e65ed835c",
+    "url": "/static/media/smoke-n-seoul.4aba96e9.jpg"
+  }
+]);
